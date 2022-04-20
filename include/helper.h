187529042a55ecb89bb123420bffa7bdee1abf78
ColorsWind <14761584+ColorsWind@users.noreply.github.com>
2022-04-20 21:06:55 +0800

    improv(Helper): Suppress ffmpeg unnecessary warning in Windows

--- include/helper.h
+++ include/helper.h
@@ -5,13 +5,23 @@
 #ifndef PONYPLAYER_HELPER_H
 #define PONYPLAYER_HELPER_H
 
-
+#ifdef WIN32
+// windows
+#define INCLUDE_FFMPEG_BEGIN \
+extern "C" {       \
+_Pragma("GCC diagnostic push") \
+_Pragma("GCC diagnostic ignored \"-Wold-style-cast\"") \
+_Pragma("GCC diagnostic ignored \"-Wsign-conversion\"")
+_Pragma("GCC diagnostic ignored \"-Wconversion\"")
+#else
+// unix
 #define INCLUDE_FFMPEG_BEGIN \
 extern "C" {       \
 _Pragma("GCC diagnostic push") \
 _Pragma("GCC diagnostic ignored \"-Wold-style-cast\"") \
 _Pragma("GCC diagnostic ignored \"-Wsign-conversion\"") \
 _Pragma("GCC diagnostic ignored \"-Wimplicit-int-conversion\"")
+#endif
 
 #define INCLUDE_FFMPEG_END \
 _Pragma("GCC diagnostic pop") \
