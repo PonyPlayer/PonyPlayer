af296a5e17508378903f4a366eb85959174623a0
ColorsWind <14761584+ColorsWind@users.noreply.github.com>
2022-04-20 18:50:51 +0800

    refactor(Launch): clean main.cpp
    Signed-off-by: ColorsWind <14761584+ColorsWind@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,9 @@
-#include <logger.h>
-#include "config.h"
+
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QApplication>
-#include "playlist.h"
-
-void hello() {
-//    (new Demuxer())->videoFrameQueueFront();
-}
-
 #include "hurricane.h"
-#include "kv_engine.h"
-#include "config.h"
-
-extern "C" {
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wold-style-cast"
-#pragma GCC diagnostic ignored "-Wsign-conversion"
-#include <libavformat/avformat.h>
-
-#pragma GCC diagnostic pop
-}
+#include "logger.h"
 
 int main(int argc, char *argv[]) {
     QQuickWindow::setGraphicsApi(QSGRendererInterface::GraphicsApi::OpenGL);
@@ -31,21 +14,13 @@ int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     qmlRegisterType<Hurricane>("Hurricane", 1, 0, "Hurricane");
     qInstallMessageHandler(logMessageHandler);
-
     const QUrl url(u"qrc:/view/main.qml"_qs);
     QQmlApplicationEngine engine;
-
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
                 if (!obj && url == objUrl)
                     QCoreApplication::exit(-1);
             }, Qt::QueuedConnection);
     engine.load(url);
-
-
-
-
-    PlayList playList("testList");
-
     return QApplication::exec();
 }
