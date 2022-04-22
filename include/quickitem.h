082a043218683e9457e83d8a513a5a77d26ae256
ColorsWind <14761584+ColorsWind@users.noreply.github.com>
2022-04-22 23:27:50 +0800

    feat(Player): test hurricane player

--- include/quickitem.h
+++ include/quickitem.h
@@ -4,42 +4,70 @@
 
 #ifndef PONYPLAYER_QUICKITEM_H
 #define PONYPLAYER_QUICKITEM_H
+
+#include <QRunnable>
+#include <QAtomicInt>
+#include <QWaitCondition>
+#include <QMutex>
+#include <QWaitCondition>
+#include <QThread>
+#include <chrono>
 #include "hurricane.h"
+#include "demuxer.h"
+
+class HurricanePlayer;
+
+using std::chrono::system_clock;
 
 class HurricaneState : public QObject {
-    Q_OBJECT
+Q_OBJECT
     QML_ELEMENT
 public:
-    /**
-     * 视频是否有效, 仅当有效时下面属性才有意义
-     */
-    bool isValid;
 
-    /**
-     * 文件名
-     */
-    QString filename;
-
-    /**
-     * 视频的帧率
-     */
-    qreal fps;
-
-    /**
-     * 文件格式
-     */
-    QString format;
 };
+
+/**
+ * 负责按时取出视频帧并设置 Image
+ */
+class VideoTimer : public QThread {
+    Q_OBJECT
+private:
+    QMutex mutex;
+    QWaitCondition condition;
+public:
+    struct NextFrameEvent {
+        QAtomicInt block = true;
+        QAtomicInteger<uint64_t> next;
+    } frameEvent;
+
+    VideoTimer(QObject *parent = nullptr);
+    void run() override;
+    void notify() {
+        condition.wakeAll();
+    }
+
+signals:
+    void timeEmit(); //NOLINT
+};
+
+
 class HurricanePlayer : public Hurricane {
     Q_OBJECT
     QML_ELEMENT
 //    Q_PROPERTY(HurricaneState state NOTIFY stateChanged)
+private:
+    Demuxer demuxer;
+    VideoTimer timer;
+    QThread timerThread;
+    clock_t videoStartTime;
+    Picture picture;
 public:
     HurricaneState state;
 
-    HurricanePlayer(QQuickItem *parent= nullptr);
+    HurricanePlayer(QQuickItem *parent = nullptr);
 
 signals:
+
     /**
      * 状态发生改变
      */
@@ -48,9 +76,11 @@ signals:
     /**
      * 视频播放完
      */
-     void playFinished();
+    void playFinished();
 
 public slots:
+
+    void playNextFrame();
     /**
      * 打开视频文件
      * @param path
@@ -84,7 +114,6 @@ public slots:
      */
     Q_INVOKABLE void seek(qreal sec);
 };
-
 
 
 #endif //PONYPLAYER_QUICKITEM_H
--- src/quickitem/player.cpp
+++ src/quickitem/player.cpp
@@ -7,25 +7,49 @@
 #include <QUrl>
 #include <QTimer>
 #include <QDir>
+#include <QThread>
+#include <QCu
+
+HurricanePlayer::HurricanePlayer(QQuickItem *parent) : Hurricane(parent) {
+    timer.start();
+}
+
 
 void HurricanePlayer::openFile(const QString &path) {
     QUrl url(path);
-    auto *timer = new QTimer(this);
-    timer->setInterval(1000/30);
-    auto *demuxer = new Demuxer;
-    demuxer->openFile(url.path().toStdString());
-    demuxer->initDemuxer();
-    connect(timer, &QTimer::timeout, [=]{
-        Picture pic = demuxer->getPicture();
-        this->setImage(pic);
-//        demuxer->videoFrameQueuePop();
-    });
-    timer->start();
+    demuxer.openFile(url.path().toStdString());
+
+    demuxer.initDemuxer();
+    connect(&timer, &VideoTimer::timeEmit, this, &HurricanePlayer::playNextFrame, Qt::BlockingQueuedConnection);
+    videoStartTime = clock();
     qDebug() << "HP: open file" << url.path();
+    emit start();
+}
+
+void HurricanePlayer::playNextFrame() {
+    qDebug() << "next frame";
+    this->setImage(picture);
+    Picture pic = demuxer.getPicture();
+    if (pic.isValid()) {
+        timer.frameEvent.block = false;
+        timer.frameEvent.next = videoStartTime + static_cast<clock_t>(pic.pts * 1000);
+        picture = pic;
+    } else {
+        timer.frameEvent.block = true;
+    }
 }
 
 void HurricanePlayer::start() {
-    qDebug() << "HP: pause start";
+    Picture pic = demuxer.getPicture();
+    if (pic.isValid()) {
+        timer.frameEvent.block = false;
+        timer.frameEvent.next = videoStartTime + static_cast<clock_t>(pic.pts * 1000);
+        timer.notify();
+    } else {
+
+    }
+
+    qDebug() << "HP: start";
 }
 
 void HurricanePlayer::pause() {
@@ -36,19 +60,6 @@ void HurricanePlayer::stop() {
     qDebug() << "HP: stop";
 }
 
-HurricanePlayer::HurricanePlayer(QQuickItem *parent) : Hurricane(parent) {
-    auto *timer = new QTimer(this);
-    timer->setInterval(1000/30);
-    auto *demuxer = new Demuxer;
-    demuxer->openFile("/Users/colorswind/Desktop/屏幕录制2022-02-16 上午9.20.24.mov");
-    demuxer->initDemuxer();
-    connect(timer, &QTimer::timeout, [=]{
-        Picture pic = demuxer->getPicture();
-        this->setImage(pic);
-//        demuxer->videoFrameQueuePop();
-    });
-    timer->start();
-}
 
 void HurricanePlayer::seek(qreal sec) {
     qDebug() << "HP: seek" << sec;
@@ -56,4 +67,26 @@ void HurricanePlayer::seek(qreal sec) {
 
 void HurricanePlayer::setSpeed(qreal speed) {
     qDebug() << "HP: setSpeed" << speed;
+}
+
+VideoTimer::VideoTimer(QObject *parent) : QThread(parent) {
+
+}
+
+void VideoTimer::run() {
+    while (true) {
+        qDebug() << "R";
+        if (frameEvent.block) {
+            mutex.lock();
+            condition.wait(&mutex);
+        } else {
+            clock_t curr = clock();
+            if (frameEvent.next > curr) {
+                uint64_t delay = frameEvent.next - curr;
+                qDebug() << delay;
+                QThread::msleep(static_cast<unsigned long>(delay));
+            }
+            emit timeEmit();
+        }
+    }
 }
