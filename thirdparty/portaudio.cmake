16a8a60814677b4952e3894e882415b4f165623e
ColorsWind <14761584+ColorsWind@users.noreply.github.com>
2022-05-03 16:57:06 +0800

    fix(portaudio): 修复 windows 下链接错误
    Signed-off-by: ColorsWind <14761584+ColorsWind@users.noreply.github.com>

--- thirdparty/portaudio.cmake
+++ thirdparty/portaudio.cmake
@@ -3,6 +3,7 @@ FetchContent_Declare(portaudio
         GIT_REPOSITORY git@github.com:PortAudio/portaudio.git
         GIT_TAG        v19.7.0
         )
+OPTION(PA_BUILD_SHARED OFF)
 FetchContent_MakeAvailable(portaudio)
 include_directories("${portaudio_SOURCE_DIR}/include")
 include_directories("${portaudio_SOURCE_DIR}/src/common")
