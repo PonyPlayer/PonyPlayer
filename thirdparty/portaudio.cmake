e957139f3af428d00c5461773a1ff1bf8a7cfdca
GnSight <ftyghome@163.com>
2022-05-03 19:46:35 +0800

    improv(portaudio):去除第三方库warning

--- thirdparty/portaudio.cmake
+++ thirdparty/portaudio.cmake
@@ -1,9 +1,15 @@
 include(FetchContent)
+set_property(
+        DIRECTORY ${portaudio_SOURCE_DIR}
+        APPEND
+        PROPERTY COMPILE_OPTIONS -w
+)
 FetchContent_Declare(portaudio
-        GIT_REPOSITORY git@github.com:PortAudio/portaudio.git
+        GIT_REPOSITORY https://github.com/PortAudio/portaudio.git
         GIT_TAG        v19.7.0
         )
 OPTION(PA_BUILD_SHARED OFF)
 FetchContent_MakeAvailable(portaudio)
+
 include_directories("${portaudio_SOURCE_DIR}/include")
 include_directories("${portaudio_SOURCE_DIR}/src/common")
