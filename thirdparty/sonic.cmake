3debe3820898d70fde8f270fe53054be03aab06a
ColorsWind <14761584+ColorsWind@users.noreply.github.com>
2022-05-04 21:30:25 +0800

    dep: 引入 sonic 库

---
+++ thirdparty/sonic.cmake
@@ -0,0 +1,7 @@
+set(sonic_SOURCE_DIR ${CMAKE_SOURCE_DIR}/thirdparty/sonic)
+add_subdirectory(${sonic_SOURCE_DIR})
+set_property(
+        DIRECTORY ${sonic_SOURCE_DIR}
+        APPEND
+        PROPERTY COMPILE_OPTIONS -w
+)
