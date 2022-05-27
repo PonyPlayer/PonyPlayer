b8df556a5ff1ac9ef36caea08b543937aea8cb7f
ColorsWind <14761584+ColorsWind@users.noreply.github.com>
2022-05-27 13:21:46 +0800

    build: 完善 windows 打包脚本
    Signed-off-by: ColorsWind <14761584+ColorsWind@users.noreply.github.com>

---
+++ src/platform.cmake
@@ -0,0 +1,62 @@
+function(windows_deploy_qt)
+    set(windeployqt "${Qt6_DIR}/../../../bin/windeployqt.exe")
+    if (CMAKE_BUILD_TYPE_UPPER STREQUAL "DEBUG")
+        list(APPEND ARGN --debug)
+    else ()
+        list(APPEND ARGN --release)
+    endif ()
+    list(APPEND ARGN --verbose 0)
+    add_custom_command(TARGET ${CMAKE_PROJECT_NAME} POST_BUILD
+        COMMAND "${windeployqt}" "$<TARGET_FILE:${PROJECT_NAME}>" ${ARGN}
+    )
+endfunction(windows_deploy_qt)
+
+
+if (WIN32)
+    # set package properties
+    set(CPACK_GENERATOR WIX ZIP)
+    set(CPACK_PACKAGE_NAME ${PROJECT_NAME})
+    set(CPACK_PACKAGE_VENDOR CS2019)
+    set(CPACK_PACKAGE_EXECUTABLES ${PROJECT_NAME};PonyPlayer)
+    set(CPACK_CREATE_DESKTOP_LINKS PonyPlayer)
+    set(CPACK_PACKAGE_INSTALL_DIRECTORY ${PROJECT_NAME})
+    set(CPACK_ARCHIVE_THREADS 0)
+    set(CPACK_WIX_UPGRADE_GUID "7A1B5C35-D464-5239-AF42-172AD856488F") # uuid("PonyPlayer")
+    install(TARGETS ${CMAKE_PROJECT_NAME} RUNTIME
+        LIBRARY DESTINATION lib
+        ARCHIVE DESTINATION lib
+        RUNTIME DESTINATION bin
+    )
+    set(CPACK_RESOURCE_FILE_LICENSE "${PONY_LICENSE}")
+    set(CPACK_PACKAGE_ICO "${PONY_ICON}")
+    include(CPACK)
+
+    # copy FFmpeg dll
+    foreach (lib_name IN LISTS FFMPEG_LIBS)
+        file(GLOB lib_source "${FFMPEG_PREFIX_PATH}/bin/${lib_name}*.dll")
+        if (NOT lib_source)
+            message(FATAL_ERROR "Cannot find: ${lib_name}.")
+        endif ()
+        get_filename_component(lib_filename "${lib_source}" NAME)
+        add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD
+            COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            "${lib_source}" "$<TARGET_FILE_DIR:${PROJECT_NAME}>"
+        )
+        install(FILES "$<TARGET_FILE_DIR:${PROJECT_NAME}>/${lib_filename}" DESTINATION bin)
+    endforeach ()
+
+    # copy Qt runtime
+    windows_deploy_qt(--qmldir "${CMAKE_SOURCE_DIR}/src/view")
+    windows_deploy_qt(
+        --qmldir "${CMAKE_SOURCE_DIR}/src/view"
+        --dir "${CMAKE_CURRENT_BINARY_DIR}/winqt_tmp/"
+    )
+    install(DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/winqt_tmp/" DESTINATION bin)
+
+    # copy Assets
+    add_custom_command(TARGET ${PROJECT_NAME} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy_directory
+        "${CMAKE_SOURCE_DIR}/assets/filters"
+        "$<TARGET_FILE_DIR:${PROJECT_NAME}>/assets/filters"
+    )
+
+endif()
