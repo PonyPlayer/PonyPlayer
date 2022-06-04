e0721d229913b79728f3fbdbca2085fa74c8cf2c
ColorsWind <14761584+ColorsWind@users.noreply.github.com>
2022-06-04 14:47:39 +0800

    test: 开启单元测试(仅启动 example)

--- src/tests/example_test.cpp
+++ src/tests/example_test.cpp
@@ -3,20 +3,18 @@
 //
 
 #include <gtest/gtest.h>
-#include "example_header.h"
-
 TEST(example_test, const_double) {
-    double res = const1d();
+    double res = 1.0;
     ASSERT_NEAR(res, 1.0, 1.0e-11) << "Should near 1.0";
 }
 
 
 TEST(example_test, const_int) {
-    int res = const1i();
+    int res = 1;
     ASSERT_EQ(res, 1) << "Should equal 1.";
 }
 
 TEST(exmaple_test, const_bool) {
-    int res = constTrue();
+    bool res = true;
     ASSERT_TRUE(res) << "Should be TRUE.";
 }
